76b2f9edbcd0cbcf59c6b75d526152d0c96fa1ca
Antonio Montalva <tonimontalvapastor@gmail.com>
2020-03-27 12:02:04 +0100

    Añado cambios

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,38 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { ToastController } from '@ionic/angular';
+
+import { AuthService } from '../services/auth.service';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
+
 export class RegistroPage implements OnInit {
 
-  email: string = "";
-  nombre: string = "";
-  apellidos: string = "";
-  fechaNacimiento: Date;
-  DNI: string = "";
-  localidad: string = "";
-  numTelf: number;
-  objetivo: string = "";
-  lesion: string = "";
-  queLesion: string = "";
-  comoNosConociste: string = "";
-  password: string = "";
-  confPassword: string = "";
+    email: string = "";
+    nombre: string = "";
+    apellidos: string = "";
+    fechaNacimiento: Date;
+    DNI: string = "";
+    localidad: string = "";
+    numTelf: number;
+    objetivo: string = "";
+    lesion: string = "";
+    queLesion: string = "";
+    comoNosConociste: string = "";
+    password: string = "";
+    confPassword: string = "";
 
-  public form = [
-    { val: 'Aumentar Volumen', isChecked: false },
-    { val: 'Perder Grasa', isChecked: false },
-    { val: 'Rehabilitación', isChecked: false },
-    { val: 'Solo practicar deporte', isChecked: false },
-    { val: 'Tonificar', isChecked: false },
-    { val: 'Otro', isChecked: false },
-  ]
+    public form = [
+      { val: 'Aumentar Volumen', isChecked: false },
+      { val: 'Perder Grasa', isChecked: false },
+      { val: 'Rehabilitación', isChecked: false },
+      { val: 'Solo practicar deporte', isChecked: false },
+      { val: 'Tonificar', isChecked: false },
+      { val: 'Otro', isChecked: false },
+    ]
 
-  constructor() { }
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, public toastController: ToastController, private _authService: AuthService, public afAuth: AngularFireAuth) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
