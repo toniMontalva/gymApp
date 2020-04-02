73364df7293447a12a5f7bf90b52866f1307a2b8
Antonio Montalva <tonimontalvapastor@gmail.com>
2020-04-02 09:35:05 +0200

    cambio interfaces

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,17 +1,28 @@
 export interface ICliente {
-    "idUser" : string,
-    "empresaAsociada" : string,
-    "nombre" : string,
-    "apellido1" : string,
-    "apellido2" : string,
-    "rol" : string,
-    "sesionesRestantes" : number,
+    "apellidos" : string,
+    // Nombre del box (en minúsculas)
+    "box" : string,
+    "dni" : string,
     "email" : string,
+    "fechaNacimiento" : string,    
+    "id" : string,
+    "localidad" : string,    
+    "nombre" : string,
+    "sesionesRestantes" : number,
+    "telefono" : string,    
+    //"rol" : string,
 }
 
 export interface IEmpresa {
-    "idEmpresa" : string,
-    "email" : string,
+    // Nombre del box(en minúsculas) #id de la empresa / no hay 2 iguales
+    "box" : string,
     "clientesEmpresa" : number,
+    "email" : string,
+    // Nombre de la empresa
+    "nombre" : string,
+    // tarifa basica 9e/mes máx 35 atletas 
+    // tarifa media 25e/mes máx 70 atletas
+    // tarifa avanzada 35 e/mes ilimitado
     "tarifa" : string,
+    "telefono" : string
 }
